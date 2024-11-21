87ff92e2e4332ae299c71d8c8f073c8d121be6d4
tangpoo <lhinav3@gmail.com>
2024-11-21 17:19:09 +0900

    data class 사용으로 equals(), hashCode() 생략

--- src/main/java/emailaddress/EmailAddress.kt
+++ src/main/java/emailaddress/EmailAddress.kt
@@ -1,28 +1,14 @@
 package emailaddress
 
-import java.util.*
-
-class EmailAddress(val localPart: String, val domain: String) {
-    override fun equals(o: Any?): Boolean {
-        if (this === o) {
-            return true
-        }
-        if (o == null || javaClass != o.javaClass) {
-            return false
-        }
-        val that = o as EmailAddress
-        return localPart == that.localPart && domain == that.domain
-    }
-
-    override fun hashCode(): Int {
-        return Objects.hash(localPart, domain)
-    }
+data class EmailAddress(val localPart: String,
+                   val domain: String) {
 
     override fun toString(): String {
         return "$localPart@$domain"
     }
 
     companion object {
+        @JvmStatic
         fun parse(value: String): EmailAddress {
             val atIndex = value.lastIndexOf('@')
             require(!(atIndex < 1 || atIndex == value.length - 1)) { "EmailAddress must be two parts separated by @" }
