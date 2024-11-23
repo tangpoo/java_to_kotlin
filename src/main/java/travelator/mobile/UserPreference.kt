c13c8a1dc04f97fedd7753cde220425d880cf6c3
tangpoo <lhinav3@gmail.com>
2024-11-23 15:02:05 +0900

    UserPreference Kotlin Convert

--- src/main/java/travelator/mobile/UserPreference.kt
+++ src/main/java/travelator/mobile/UserPreference.kt
@@ -1,45 +1,9 @@
-package travelator.mobile;
+package travelator.mobile
 
-import java.util.Currency;
-import java.util.Locale;
+import java.util.*
 
-public class UserPreference {
-
-    private String greeting;
-    private Locale locale;
-    private Currency currency;
-
-    public UserPreference() {
-        this("Hello", Locale.UK, Currency.getInstance(Locale.UK));
-    }
-
-    public UserPreference(final String greeting, final Locale locale, final Currency currency) {
-        this.greeting = greeting;
-        this.locale = locale;
-        this.currency = currency;
-    }
-
-    public String getGreeting() {
-        return greeting;
-    }
-
-    public Locale getLocale() {
-        return locale;
-    }
-
-    public Currency getCurrency() {
-        return currency;
-    }
-
-    public void setGreeting(final String greeting) {
-        this.greeting = greeting;
-    }
-
-    public void setLocale(final Locale locale) {
-        this.locale = locale;
-    }
-
-    public void setCurrency(final Currency currency) {
-        this.currency = currency;
-    }
-}
+class UserPreference @JvmOverloads constructor(
+        var greeting: String = "Hello",
+        var locale: Locale = Locale.UK,
+        var currency: Currency = Currency.getInstance(Locale.UK)
+)
