361e5c8387cf6a7e5b690f2ee8b5b2c6c1191b6d
tangpoo <lhinav3@gmail.com>
2024-11-24 22:29:27 +0900

    모든 함수 리팩터링 / bearable kotlin의 filter 함수로 간결화 / Java의 컬렉션을 Kotlin이 변경할 수 있고, Kotlin의 컬렉션을 Java가 변경할 수 있다는 점을 기억하라. / Java의 컬렉션을 사용하는 코드에서 내부 상태 변경을 제거하라. 불가능하다면 방어적 복사를 수행하라.

--- src/main/java/travelator/suffer/Suffering.kt
+++ src/main/java/travelator/suffer/Suffering.kt
@@ -1,35 +1,26 @@
 package travelator.suffer
 
-import java.util.stream.Collectors
-
 object Suffering {
 
     @JvmStatic
-    fun sufferScoreFor(route: List<Journey>): Int {
-        return sufferScore(
-                route.longestJourneysIn(limit = 3),
-                Routes.getDepartsFrom(route))
-    }
+    fun sufferScoreFor(route: List<Journey>): Int =
+            sufferScore(
+                    route.longestJourneysIn(limit = 3),
+                    Routes.getDepartsFrom(route))
 
     @JvmStatic
     fun List<Journey>.longestJourneysIn(limit: Int): List<Journey> =
             sortedByDescending { it.duration }.take(limit)
 
-    fun routesToShowFor(itineraryId: String?): List<List<Journey>> {
-        return bearable(Other.routesFor(itineraryId))
-    }
+    fun routesToShowFor(itineraryId: String?): List<List<Journey>> =
+            bearable(Other.routesFor(itineraryId))
 
-    private fun bearable(routes: List<List<Journey>>
-    ): List<List<Journey>> {
-        return routes.stream()
-                .filter { route: List<Journey> -> sufferScoreFor(route) <= 10 }
-                .collect(Collectors.toUnmodifiableList())
-    }
 
-    private fun sufferScore(
-            longestJourneys: List<Journey>,
-            start: Location
-    ): Int {
-        return Other.SOME_COMPLICATED_RESULT()
-    }
+    private fun bearable(routes: List<List<Journey>>): List<List<Journey>> =
+            routes.filter { sufferScoreFor(it) <= 10 }
+
+
+    private fun sufferScore(longestJourneys: List<Journey>, start: Location): Int =
+            Other.SOME_COMPLICATED_RESULT()
+
 }
