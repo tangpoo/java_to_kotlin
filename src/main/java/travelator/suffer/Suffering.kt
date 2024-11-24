580df09be844bb77f7965f1ebdd4eb2904159a6b
tangpoo <lhinav3@gmail.com>
2024-11-24 21:55:48 +0900

    longestJourneysIn 함수 리팩터링 / sortedByDescending 사용으로 List sorting 과정 간결화 / 확장 함수 사용 / 파라미터를 변경하지 않는 함수이기 때문에 단일식 함수 적용 가능

--- src/main/java/travelator/suffer/Suffering.kt
+++ src/main/java/travelator/suffer/Suffering.kt
@@ -1,28 +1,19 @@
 package travelator.suffer
 
 import java.util.stream.Collectors
-import kotlin.math.min
 
 object Suffering {
 
     @JvmStatic
     fun sufferScoreFor(route: List<Journey>): Int {
         return sufferScore(
-                longestJourneysIn(route, 3),
+                route.longestJourneysIn(3),
                 Routes.getDepartsFrom(route))
     }
 
     @JvmStatic
-    fun longestJourneysIn(
-            journeys: List<Journey>,
-            limit: Int
-    ): List<Journey> {
-        val actualLimit = min(journeys.size.toDouble(), limit.toDouble()).toInt()
-        return Collections.sorted(
-                journeys,
-                Comparator.comparing { obj: Journey -> obj.duration }.reversed()
-        ).subList(0, actualLimit)
-    }
+    fun List<Journey>.longestJourneysIn(limit: Int): List<Journey> =
+            sortedByDescending { it.duration }.take(limit)
 
     fun routesToShowFor(itineraryId: String?): List<List<Journey>> {
         return bearable(Other.routesFor(itineraryId))
