b4b347fddaa088e106d7041b783c3271b4420646
tangpoo <lhinav3@gmail.com>
2024-11-24 21:47:09 +0900

    Convert Kotlin

--- src/main/java/travelator/suffer/Suffering.kt
+++ src/main/java/travelator/suffer/Suffering.kt
@@ -1,44 +1,44 @@
-package travelator.suffer;
+package travelator.suffer
 
-import static java.util.Comparator.comparing;
-import static java.util.stream.Collectors.toUnmodifiableList;
-import static travelator.suffer.Collections.sorted;
-import static travelator.suffer.Other.SOME_COMPLICATED_RESULT;
-import static travelator.suffer.Other.routesFor;
-import static travelator.suffer.Routes.getDepartsFrom;
+import java.util.stream.Collectors
+import kotlin.math.min
 
-import java.util.List;
+object Suffering {
 
-public class Suffering {
-    public static int sufferScoreFor(List<Journey> route) {
+    @JvmStatic
+    fun sufferScoreFor(route: List<Journey>): Int {
         return sufferScore(
-            longestJourneysIn(route, 3),
-            getDepartsFrom(route));
+                longestJourneysIn(route, 3),
+                Routes.getDepartsFrom(route))
     }
-    static List<Journey> longestJourneysIn(
-        List<Journey> journeys,
-        int limit
-    ) {
-        var actualLimit = Math.min(journeys.size(), limit);
-        return sorted(
-            journeys,
-            comparing(Journey::getDuration).reversed()
-        ).subList(0, actualLimit);
+
+    @JvmStatic
+    fun longestJourneysIn(
+            journeys: List<Journey>,
+            limit: Int
+    ): List<Journey> {
+        val actualLimit = min(journeys.size.toDouble(), limit.toDouble()).toInt()
+        return Collections.sorted(
+                journeys,
+                Comparator.comparing { obj: Journey -> obj.duration }.reversed()
+        ).subList(0, actualLimit)
     }
-    public static List<List<Journey>> routesToShowFor(String itineraryId) {
-        return bearable(routesFor(itineraryId));
+
+    fun routesToShowFor(itineraryId: String?): List<List<Journey>> {
+        return bearable(Other.routesFor(itineraryId))
     }
-    private static List<List<Journey>> bearable
-        (List<List<Journey>> routes
-        ) {
+
+    private fun bearable(routes: List<List<Journey>>
+    ): List<List<Journey>> {
         return routes.stream()
-            .filter(route -> sufferScoreFor(route) <= 10)
-            .collect(toUnmodifiableList());
+                .filter { route: List<Journey> -> sufferScoreFor(route) <= 10 }
+                .collect(Collectors.toUnmodifiableList())
     }
-    private static int sufferScore(
-        List<Journey> longestJourneys,
-        Location start
-    ) {
-        return SOME_COMPLICATED_RESULT();
+
+    private fun sufferScore(
+            longestJourneys: List<Journey>,
+            start: Location
+    ): Int {
+        return Other.SOME_COMPLICATED_RESULT()
     }
 }
