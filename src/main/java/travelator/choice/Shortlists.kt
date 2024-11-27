b89e6737329de89e406fee0c8a12680d6905f408
tangpoo <lhinav3@gmail.com>
2024-11-27 20:55:32 +0900

    Convert Kotlin / 이 클래스는 비정적 메서드나 상태가 없기 때문에 싱글턴 객체(object)로 변환 된다. / 앞서 정리한 내용에 따라 클래스를 제거하고 이 함수들을 최상위 함수로 변환할 필요가 있다.

--- src/main/java/travelator/choice/Shortlists.kt
+++ src/main/java/travelator/choice/Shortlists.kt
@@ -1,38 +1,42 @@
-package travelator.choice;
+package travelator.choice
 
-import static java.util.Comparator.comparing;
-import static java.util.Comparator.comparingDouble;
-import static java.util.stream.Collectors.toUnmodifiableList;
+import java.util.stream.Collectors
+import java.util.stream.Stream
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Stream;
-
-public class Shortlists {
-    public static <T> List<T> sorted(
-        List<T> shortlist,
-        Comparator<? super T> ordering
-    ) {
+object Shortlists {
+    fun <T> sorted(
+            shortlist: List<T>,
+            ordering: Comparator<in T>?
+    ): List<T> {
         return shortlist.stream()
-            .sorted(ordering)
-            .collect(toUnmodifiableList());
+                .sorted(ordering)
+                .collect(Collectors.toUnmodifiableList())
     }
-    public static <T> List<T> removeItemAt(List<T> shortlist, int index) {
+
+    fun <T> removeItemAt(shortlist: List<T>, index: Int): List<T> {
         return Stream.concat(
-            shortlist.stream().limit(index),
-            shortlist.stream().skip(index + 1)
-        ).collect(toUnmodifiableList());
+                shortlist.stream().limit(index.toLong()),
+                shortlist.stream().skip((index + 1).toLong())
+        ).collect(Collectors.toUnmodifiableList())
     }
-    public static Comparator<HasRating> byRating() {
-        return comparingDouble(HasRating::getRating).reversed();
+
+    @JvmStatic
+    fun byRating(): Comparator<HasRating> {
+        return Comparator.comparingDouble(HasRating::rating).reversed()
     }
-    public static Comparator<HasPrice> byPriceLowToHigh() {
-        return comparing(HasPrice::getPrice);
+
+    @JvmStatic
+    fun byPriceLowToHigh(): Comparator<HasPrice> {
+        return Comparator.comparing(HasPrice::price)
     }
-    public static <T extends HasPrice & HasRating> Comparator<T> byValue() {
-        return comparingDouble((T t) -> t.getRating() / t.getPrice()).reversed();
+
+    @JvmStatic
+    fun <T> byValue(): Comparator<T> where T : HasPrice?, T : HasRating? {
+        return Comparator.comparingDouble { t: T -> t!!.rating / t.price }.reversed()
     }
-    public static Comparator<HasRelevance> byRelevance() {
-        return comparingDouble(HasRelevance::getRelevance).reversed();
+
+    @JvmStatic
+    fun byRelevance(): Comparator<HasRelevance> {
+        return Comparator.comparingDouble(HasRelevance::relevance).reversed()
     }
 }
