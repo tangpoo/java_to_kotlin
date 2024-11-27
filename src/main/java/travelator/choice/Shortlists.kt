b7f7e1db7367ec34fe1074db613a1ada7f17dd46
tangpoo <lhinav3@gmail.com>
2024-11-27 21:11:43 +0900

    @file:JvmName("Shortlists")을 사용해 JVM에게 해당 클래스의 내부 메서드인 것 처럼 인식 시킬 수 있다. / 그러나 이를 참조했던 테스트에서 컴파일 에러가 발생한다. / 해당 방식으로는 리팩터링으로 인한 참조 변화를 다른 코드에 전파할 수 없다.

--- src/main/java/travelator/choice/Shortlists.kt
+++ src/main/java/travelator/choice/Shortlists.kt
@@ -1,42 +1,44 @@
+@file:JvmName("Shortlists")
+
 package travelator.choice
 
 import java.util.stream.Collectors
 import java.util.stream.Stream
 
-object Shortlists {
-    fun <T> sorted(
-            shortlist: List<T>,
-            ordering: Comparator<in T>?
-    ): List<T> {
-        return shortlist.stream()
-                .sorted(ordering)
-                .collect(Collectors.toUnmodifiableList())
-    }
 
-    fun <T> removeItemAt(shortlist: List<T>, index: Int): List<T> {
-        return Stream.concat(
-                shortlist.stream().limit(index.toLong()),
-                shortlist.stream().skip((index + 1).toLong())
-        ).collect(Collectors.toUnmodifiableList())
-    }
-
-    @JvmStatic
-    fun byRating(): Comparator<HasRating> {
-        return Comparator.comparingDouble(HasRating::rating).reversed()
-    }
-
-    @JvmStatic
-    fun byPriceLowToHigh(): Comparator<HasPrice> {
-        return Comparator.comparing(HasPrice::price)
-    }
-
-    @JvmStatic
-    fun <T> byValue(): Comparator<T> where T : HasPrice?, T : HasRating? {
-        return Comparator.comparingDouble { t: T -> t!!.rating / t.price }.reversed()
-    }
-
-    @JvmStatic
-    fun byRelevance(): Comparator<HasRelevance> {
-        return Comparator.comparingDouble(HasRelevance::relevance).reversed()
-    }
+fun <T> sorted(
+        shortlist: List<T>,
+        ordering: Comparator<in T>?
+): List<T> {
+    return shortlist.stream()
+            .sorted(ordering)
+            .collect(Collectors.toUnmodifiableList())
 }
+
+fun <T> removeItemAt(shortlist: List<T>, index: Int): List<T> {
+    return Stream.concat(
+            shortlist.stream().limit(index.toLong()),
+            shortlist.stream().skip((index + 1).toLong())
+    ).collect(Collectors.toUnmodifiableList())
+}
+
+
+fun byRating(): Comparator<HasRating> {
+    return Comparator.comparingDouble(HasRating::rating).reversed()
+}
+
+
+fun byPriceLowToHigh(): Comparator<HasPrice> {
+    return Comparator.comparing(HasPrice::price)
+}
+
+
+fun <T> byValue(): Comparator<T> where T : HasPrice?, T : HasRating? {
+    return Comparator.comparingDouble { t: T -> t!!.rating / t.price }.reversed()
+}
+
+
+fun byRelevance(): Comparator<HasRelevance> {
+    return Comparator.comparingDouble(HasRelevance::relevance).reversed()
+}
+
