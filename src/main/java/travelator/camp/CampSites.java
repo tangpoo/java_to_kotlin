24244b81a8daf7b34524f0d5d5ed7a0c93a51053
tangpoo <lhinav3@gmail.com>
2024-12-01 18:47:59 +0900

    메서드를 프로퍼티로 BaseCode

---
+++ src/main/java/travelator/camp/CampSites.java
@@ -0,0 +1,20 @@
+package travelator.camp;
+
+import static java.util.stream.Collectors.toUnmodifiableSet;
+
+import java.util.Set;
+
+public class CampSites {
+    public static Set<CampSite> sitesInRegion(
+        Set<CampSite> sites,
+        String countryISO,
+        String region
+    ) {
+        return sites.stream()
+            .filter( campSite ->
+                campSite.getCountryCode().equals(countryISO) &&
+                    campSite.region().equalsIgnoreCase(region)
+            )
+            .collect(toUnmodifiableSet());
+    }
+}
