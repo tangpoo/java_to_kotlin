9dce41b4ad9d3935352fbb27d2651164650391ea
tangpoo <lhinav3@gmail.com>
2024-12-01 18:59:41 +0900

    IntelliJ가 제안한 Kotlin 코스 스타일 적용

--- src/main/java/travelator/marketing/HighValueCustomersReport.kt
+++ src/main/java/travelator/marketing/HighValueCustomersReport.kt
@@ -9,8 +9,8 @@ import java.util.*
 @Throws(IOException::class)
 fun generate(reader: Reader, writer: Writer) {
     val valuableCustomers = reader.readLines()
-            .toValuableCustomers()
-            .sortedBy(CustomerData::score)
+        .toValuableCustomers()
+        .sortedBy(CustomerData::score)
     writer.appendLine("ID\tName\tSpend\n")
     for (customerData in valuableCustomers) {
         writer.appendLine(customerData.outputLine)
@@ -19,26 +19,26 @@ fun generate(reader: Reader, writer: Writer) {
 }
 
 private fun List<String>.toValuableCustomers() = withoutHeader()
-        .map(String::toCustomerData)
-        .filter { it.score >= 10 }
+    .map(String::toCustomerData)
+    .filter { it.score >= 10 }
 
 private fun List<String>.withoutHeader() = drop(1)
 
 private fun List<CustomerData>.summarised(): String =
-        sumOf { it.spend }.let { total ->
-            "\tTOTAL\t" + total.toMoneyString()
-        }
+    sumOf { it.spend }.let { total ->
+        "\tTOTAL\t" + total.toMoneyString()
+    }
 
 fun String.toCustomerData(): CustomerData =
-        split("\t").let { parts ->
-            CustomerData(
-                    id = parts[0],
-                    givenName = parts[1],
-                    familyName = parts[2],
-                    score = parts[3].toInt(),
-                    spend = if (parts.size == 4) 0.0 else parts[4].toDouble()
-            )
-        }
+    split("\t").let { parts ->
+        CustomerData(
+            id = parts[0],
+            givenName = parts[1],
+            familyName = parts[2],
+            score = parts[3].toInt(),
+            spend = if (parts.size == 4) 0.0 else parts[4].toDouble()
+        )
+    }
 
 private val CustomerData.outputLine: String
     get() = "$id\t$marketingName\t${spend.toMoneyString()}"
