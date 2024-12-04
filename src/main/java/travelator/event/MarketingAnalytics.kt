0033b8b1a79476200dcb804718b0c6e04f6652c9
tangpoo <lhinav3@gmail.com>
2024-12-04 23:29:37 +0900

    2.시퀀스로 리팩터링 / 타입 별명과 확장 프로퍼티를 사용해 groupBy 안에서 리스트를 만드는 잡음을 제거

--- src/main/java/travelator/event/MarketingAnalytics.kt
+++ src/main/java/travelator/event/MarketingAnalytics.kt
@@ -7,22 +7,14 @@ class MarketingAnalytics(
 ) {
     fun averageNumberOfEventsPerCompletedBooking(
         timeRange: String
-    ): Double {
-        return allEventsInSameInteractions(
-            eventStore
-                .queryAsSequence("type=CompletedBooking&timerange=$timeRange")
-        )
-            .groupBy { event ->
-                event["interactionId"] as String
-            }
-            .values
-            .averageBy {it.size}
-    }
+    ): Double = eventStore
+        .queryAsSequence("type=CompletedBooking&timerange=$timeRange")
+        .allEventsInSameInteractions()
+        .groupBy(Event::interactionId)
+        .values
+        .averageBy {it.size}
 
-    private fun allEventsInSameInteractions(
-        sequence: Sequence<MutableMap<String, Any>>
-                                            ): Sequence<MutableMap<String, Any>> = sequence
-        .flatMap { event ->
+    private fun Sequence<MutableMap<String, Any>>.allEventsInSameInteractions(): Sequence<MutableMap<String, Any>> = flatMap { event ->
             val interactionId = event["interactionId"] as String
             eventStore
                 .queryAsSequence("interactionId=$interactionId")
@@ -34,3 +26,8 @@ class MarketingAnalytics(
 
 fun EventStore.queryAsSequence(query: String) =
     this.queryAsStream(query).asSequence()
+
+typealias Event = Map<String, Any?>
+
+val Event.interactionId: String? get() =
+    this["interactionId"] as? String
