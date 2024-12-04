f5e9ee8537e4ff70ccb9f10ffed517fadefc71ac
tangpoo <lhinav3@gmail.com>
2024-12-04 19:59:06 +0900

    2.시퀀스로 리팩터링 / Java의 스트림을 인메모리는 이터러블로, 크기를 알 수 없는 컬렉션 연산에는 시퀀스로 리팩터링 했지만, 여전히 코드가 복잡하다. / 모든 코드를 인라이닝 함으로써 추출할 동작을 분석할 수 있다.

--- src/main/java/travelator/event/MarketingAnalytics.kt
+++ src/main/java/travelator/event/MarketingAnalytics.kt
@@ -10,18 +10,18 @@ class MarketingAnalytics(
     fun averageNumberOfEventsPerCompletedBooking(
         timeRange: String
     ): Double {
-        val eventsForSuccessfulBookings = eventStore
+        return eventStore
                 .queryAsSequence("type=CompletedBooking&timerange=$timeRange")
                 .flatMap { event ->
                     val interactionId = event["interactionId"] as String?
                     eventStore
                         .queryAsSequence("interactionId=$interactionId")
                 }
-        val bookingEventsByInteractionId = eventsForSuccessfulBookings
             .groupBy { event ->
                 event["interactionId"] as String
             }
-        return bookingEventsByInteractionId.values.averageBy {it.size}
+            .values
+            .averageBy {it.size}
     }
 
     private fun <T> Collection<T>.averageBy(selector: (T) -> Int): Double =
