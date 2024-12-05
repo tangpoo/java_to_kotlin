8fe34c8eae9edec3a656d14d0dff32a9ed5f2dbb
tangpoo <lhinav3@gmail.com>
2024-12-06 01:18:19 +0900

    부 생성자를 포함하도록 주 생성자를 변경하고 부 생성자를 지운다. / 하지만 내부에서 여전히 var, init과 같은 가변성에 의존하고 있기에 제거하기가 힘들다. / fold는 이런 가변 변수에 대한 동작을 불변 변수를 초기화하기 위한 계산으로 변환해 준다.

--- src/main/java/travelator/itinerary/CostSummary.kt
+++ src/main/java/travelator/itinerary/CostSummary.kt
@@ -5,23 +5,16 @@ import travelator.money.Money
 import travelator.money.Money.Companion.of
 import java.util.*
 
-class CostSummary(userCurrency: Currency) {
-    private val _lines = mutableListOf<CurrencyConversion>()
+class CostSummary(
+    userCurrency: Currency,
+    val lines: List<CurrencyConversion>
+) {
     var total: Money = of(0, userCurrency)
         private set
 
-    val lines: List<CurrencyConversion>
-        get() = _lines.toList()
-
-    constructor(
-        userCurrency: Currency,
-        lines: List<CurrencyConversion>
-    ): this(userCurrency) {
-        lines.forEach(::addLine)
-    }
-
-    fun addLine(line: CurrencyConversion) {
-        _lines.add(line)
-        total += line.toMoney
+    init {
+        lines.forEach {
+            total += it.toMoney
+        }
     }
 }
