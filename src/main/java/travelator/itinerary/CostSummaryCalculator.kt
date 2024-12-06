04369186058d00d6b957c870dd4795099d4fe3ed
tangpoo <lhinav3@gmail.com>
2024-12-06 22:13:51 +0900

    사용되지 않는 코드 제거

--- src/main/java/travelator/itinerary/CostSummaryCalculator.kt
+++ src/main/java/travelator/itinerary/CostSummaryCalculator.kt
@@ -9,22 +9,6 @@ class CostSummaryCalculator(
     private val userCurrency: Currency,
     private val exchangeRates: ExchangeRates
 ) {
-    private val currencyTotals: MutableMap<Currency, Money> = HashMap()
-
-    fun addCost(cost: Money) {
-        currencyTotals.merge(cost.currency, cost, Money::add)
-    }
-
-    fun summarise(): CostSummary {
-        val conversions = currencyTotals.values.sortedBy {
-            it.currency.currencyCode
-        }.map { exchangeRates.convert(it, userCurrency) }
-
-        return CostSummary(conversions, conversions
-            .map { it.toMoney }
-            .fold(Money.of(0, userCurrency), Money::add))
-    }
-
     fun summarise(costs: Iterable<Money>): CostSummary {
         val currencyTotals: List<Money> = costs
             .groupBy { it.currency }
@@ -37,9 +21,5 @@ class CostSummaryCalculator(
             .map { it.toMoney }
             .fold(Money(0, userCurrency), Money::add)
         return CostSummary(lines, total)
-    }
-
-    fun reset() {
-        currencyTotals.clear()
     }
 }
