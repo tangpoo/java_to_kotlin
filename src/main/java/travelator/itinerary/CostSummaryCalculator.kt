f7e9a2f9024ac990930105f2dab437bd5605bde3
tangpoo <lhinav3@gmail.com>
2024-12-06 21:41:33 +0900

    지역 변수에 CostSummaryCalculator를 지정하며 기존 함수를 호출하는 방식 대신, 새로운 summarise 메서드에 모든 클래스를 인라이닝 / 한 메서드 내부에 모든 가변 상태를 집약할 수 있다.

--- src/main/java/travelator/itinerary/CostSummaryCalculator.kt
+++ src/main/java/travelator/itinerary/CostSummaryCalculator.kt
@@ -25,9 +25,17 @@ class CostSummaryCalculator(
     }
 
     fun summarise(costs: Iterable<Money>): CostSummary {
-        val delegate = CostSummaryCalculator(userCurrency, exchangeRates)
-        costs.forEach(delegate::addCost)
-        return delegate.summarise()
+        val currencyTotals = mutableMapOf<Currency, Money>()
+        costs.forEach {
+            currencyTotals.merge(it.currency,it, Money::plus)
+        }
+        val lines = currencyTotals.values
+            .sortedBy { it.currency.currencyCode }
+            .map { exchangeRates.convert(it, userCurrency) }
+        val total = lines
+            .map { it.toMoney }
+            .fold(Money(0, userCurrency), Money::add)
+        return CostSummary(lines, total)
     }
 
     fun reset() {
