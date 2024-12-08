874e2505bdc6aad7de55e76b2d17794709b2887e
tangpoo <lhinav3@gmail.com>
2024-12-09 00:40:19 +0900

    추상 메서드가 하나라면 fun interface 를 활용해 람다 단일식으로 유일한 연산을 정의할 수 있다. / 구태여 구현체 클래스를 생성하지 않아도 되는 장점이 있다. / createEmailSender는 ISendEmail send 메서드의 구현체와 같이 동작하며, 이 함수를 통해 반환된 ISendEmail은 Email 인자값과 저장하고 있는 인증 정보를 통해 sendEmail 함수를 실행할 수 있다. / 이를 '함수를 둘러싼 함수'라고 표현하며, 둘러싼 함수의 파라미터를 포획하는 '클로저'라고도 부른다.

--- src/main/java/travelator/emailaddress/EmailSender.kt
+++ src/main/java/travelator/emailaddress/EmailSender.kt
@@ -2,59 +2,29 @@ package travelator.emailaddress
 
 import java.net.InetAddress
 
-interface ISendEmail {
+fun interface ISendEmail {
     fun send(email: Email)
 }
 
-class EmailSender(
-    private val serverAddress: InetAddress,
-    private val username: String,
-    private val password: String
-) : ISendEmail {
-    override fun send(email: Email) {
-        sendEmail(
-            email,
-            serverAddress,
-            username,
-            password
-        )
-    }
-
+fun createEmailSender(
+    serverAddress: InetAddress,
+    username: String,
+    password: String
+) = ISendEmail { email ->
+    sendEmail(
+        email,
+        serverAddress,
+        username,
+        password
+    )
 }
 
-fun EmailSender.sendEmail(
+fun sendEmail(
     email: Email,
     serverAddress: InetAddress,
     username: String,
     password: String
 ) {
 
-}
-
-
-//설정을 알 수 있는 곳
-val subsystem = Rescuing(
-    EmailSender(
-        inetAddress("smtp.travelator.com"),
-        "username",
-        "password"
-    )
-)
-
-
-// 메세지를 보내는 곳
-class Rescuing(
-    private val sender: EmailSender
-) {
-    fun sendThanks() {
-        sender.send(
-            Email(
-                to = EmailAddress.parse("support@internationalrescue.org"),
-                from = EmailAddress.parse("support@travelator.com"),
-                subject = "Thanks for your help",
-                body = "..."
-            )
-        )
-    }
 }
 
