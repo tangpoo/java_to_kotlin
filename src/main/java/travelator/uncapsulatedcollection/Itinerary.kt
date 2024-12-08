544d5f60f2a9c7be0ce22a6e8f3f833464f2f4ef
tangpoo <lhinav3@gmail.com>
2024-12-08 16:44:38 +0900

    이런 경우 위임을 사용해 Itinerary를 Route 처럼 다룰 수 있다. / hasJourneyLongerThan 매서드를 밖으로 빼고 Route의 확장 함수로 변경하여 이를 모든 Route에서도 사용할 수 있도록 한다. / Route에 관한 확장을 이제 Itinerary에서도 사용이 가능하다.

--- src/main/java/travelator/uncapsulatedcollection/Itinerary.kt
+++ src/main/java/travelator/uncapsulatedcollection/Itinerary.kt
@@ -5,8 +5,22 @@ import java.time.Duration
 class Itinerary(
     val id: Id<Itinerary>,
     val route: Route
-) {
+) : Route by route {
 
-    fun hasJourneyLongerThan(duration: Duration) =
-        route.any{ it.duration > duration }
 }
+
+fun Iterable<Itinerary>.shortest() =
+    minByOrNull {
+        it.duration
+    }
+
+fun Route.hasJourneyLongerThan(duration: Duration) =
+    any{ it.duration > duration }
+
+
+
+fun Itinerary.withoutJourneysBy(travelMethod: TravelMethod) =
+    Itinerary(
+        id,
+        this.filterNot { it.method == travelMethod }
+    )
