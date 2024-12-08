fa3ab59f9257c680e5357ecc10dc83e57caac5c4
tangpoo <lhinav3@gmail.com>
2024-12-08 15:54:38 +0900

    Itinerary와 같이 다른 프로퍼티가 함께 존재하는 경우 타입 별명을 사용할 수 없다.

--- src/main/java/travelator/uncapsulatedcollection/Itinerary.kt
+++ src/main/java/travelator/uncapsulatedcollection/Itinerary.kt
@@ -1,7 +1,12 @@
 package travelator.uncapsulatedcollection
 
+import java.time.Duration
+
 class Itinerary(
     val id: Id<Itinerary>,
     val route: Route
 ) {
+
+    fun hasJourneyLongerThan(duration: Duration) =
+        route.any{ it.duration > duration }
 }
