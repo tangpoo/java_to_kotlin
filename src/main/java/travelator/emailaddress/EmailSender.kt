7fc694aee7e28fb92726e19a5e8f9e78442f022e
tangpoo <lhinav3@gmail.com>
2024-12-09 19:19:06 +0900

    객체지향의 세부사항 캡슐화 방식을 알아봤다. / 함수형의 세부사항 캡슐화를 알아보자. / 인자 값 중 일부를 고정시키며 인자가 더 적은 새 함수를 만들어내는 것을 '부분적용' 이라고 한다. / createEmailSender는 클래스는 아니지만, 파라미터를 포함한 함수의 생성자와 같이 동작한다. / object 정의와 함수형 클로저, fun interface와 매우 비슷하다. / 암시적 invoke 대신 send가 숨어있는 차이.

--- src/main/java/travelator/emailaddress/EmailSender.kt
+++ src/main/java/travelator/emailaddress/EmailSender.kt
@@ -2,22 +2,19 @@ package travelator.emailaddress
 
 import java.net.InetAddress
 
-fun interface ISendEmail {
-    fun send(email: Email)
-}
-
 fun createEmailSender(
     serverAddress: InetAddress,
     username: String,
     password: String
-) = ISendEmail { email ->
-    sendEmail(
-        email,
-        serverAddress,
-        username,
-        password
-    )
-}
+): (Email) -> Unit =
+    { email ->
+        sendEmail(
+            email,
+            serverAddress,
+            username,
+            password
+        )
+    }
 
 fun sendEmail(
     email: Email,
