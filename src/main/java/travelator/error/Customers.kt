aa3c6faf51c031b5f724dd5f40a7a163fec0d220
tangpoo <lhinav3@gmail.com>
2024-12-16 20:04:34 +0900

    Result4k 의존성 추가. / Result 타입을 반환하는 add 함수의 새로운 버전을 만든다. / Java와의 상호 운용성을 유지하기 위해 DuplicateException 을 사용한다. / 모든 코드가 새로운 함수를 사용하면 기존 add 함수를 제거하는 '확장과 축소 리팩터링' 방식을 사용한다. / 예외를 돌려주는 메서드에서 결과를 돌려주는 메서드로 마이그레이션 할 수 있다.

--- src/main/java/travelator/error/Customers.kt
+++ src/main/java/travelator/error/Customers.kt
@@ -1,9 +1,19 @@
 package travelator.error
 
+import dev.forkhandles.result4k.Failure
+import dev.forkhandles.result4k.Result
+import dev.forkhandles.result4k.Success
 import java.util.*
 
 interface Customers {
     @Throws(DuplicateException::class)
     fun add(name: String, email: String): Customer
+    fun addToo(name: String, email: String)
+    :Result<Customer, DuplicateException> =
+        try {
+            Success(add(name, email))
+        } catch (x: DuplicateException) {
+            Failure(x)
+        }
     fun find(id: String): Optional<Customer>
 }
