a265af1540c9ce2f9d71019299edf9154e2c7fe7
tangpoo <lhinav3@gmail.com>
2024-12-17 22:31:09 +0900

    계층이라는 관점에서 Cutomers는 Registration보다 낮은 계층이다. / 낮은 계층이 상위 계층에 의존하지 말아야 한다. / Repository 계층인 Customers에 Duplicate 클래스를 추가하는 것은 더 이상 에러를 추가할 수 없게 함으로써 데이터베이스와 상호작용 중에 발생하는 에러를 추가할 수 없게 된다. / 이는 또 다시 두 개 이상 예외를 던지는 메서드를 어떻게 표현하는가?에 대한 질문이 된다. / Customers에 CustomerProblem 봉인 클래스를 추가하고 하위 클래스를 정의한다. / add 메서드는 DuplicateCustomerProblem을 반환할 수 있도록 변한한다.

--- src/main/java/travelator/error/Customers.kt
+++ src/main/java/travelator/error/Customers.kt
@@ -6,6 +6,12 @@ import dev.forkhandles.result4k.Success
 import java.util.*
 
 interface Customers {
-    fun add(name: String, email: String):Result<Customer, DuplicateException>
+    fun add(name: String, email: String):Result<Customer, CustomerProblem>
     fun find(id: String): Optional<Customer>
 }
+
+sealed class CustomerProblem
+
+data class DuplicateCustomerProblem(val message: String): CustomerProblem()
+
+data class DatabaseCustomerProblem(val message: String): CustomerProblem()
