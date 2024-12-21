48015378c6dacbe21f39b08b97eb616f84a91069
tangpoo <lhinav3@gmail.com>
2024-12-21 22:33:42 +0900

    문자열 데이터를 파싱하여 우리가 원하는 타입의 컬렉션으로 변환하는 테스트와 함수를 작성해보자. / Java라면 TableReader 클래스부터 만들기 시작할 것이다. / 하지만 우리가 원하는 것은 입력 여러 줄을 우리가 원하는 데이터로 변환하는 계산이다. / 따라서 더 복잡한 일을 해야 할 때 까지 someFunction 같은 함수를 최상위에 선언하는 방식을 디폴트로 선택한다.

--- src/test/java/travelator/tablereader/TableReaderTests.kt
+++ src/test/java/travelator/tablereader/TableReaderTests.kt
@@ -1,10 +1,36 @@
 package travelator.tablereader
 
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class TableReaderAcceptanceTests {
-    @Test
-    fun test(){
+    data class Measurement(
+        val t: Double,
+        val x: Double,
+        val y: Double,
+    )
 
+    @Test
+    fun `acceptance test`() {
+        val input = listOf(
+            "time,x,y",
+            "0.0,  1,  1",
+            "0.1,1.1,1.2",
+            "0.2,1.2,1.4",
+        )
+        val expected = listOf(
+            Measurement(0.0, 1.0, 1.0),
+            Measurement(0.1, 1.1, 1.2),
+            Measurement(0.2, 1.2, 1.4)
+        )
+        assertEquals(
+            expected,
+            someFunctions(input)
+        )
+    }
+
+    private fun someFunctions(input: List<String>): List<Measurement> {
+        TODO("Not yet implemented")
     }
 }
